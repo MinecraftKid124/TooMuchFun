3581c9f21f9a2d4d51bff67ee322be6e2e5fafb7
MinecraftKid124 <ashtonplaymin1@gmail.com>
2024-08-11 07:18:15 +0800

    Initial commit

---
+++ src/main/java/net/ashtontink/toomuchfun/TooMuchFunClient.java
@@ -0,0 +1,10 @@
+package net.ashtontink.toomuchfun;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TooMuchFunClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Do client initialization
+    }
+}
